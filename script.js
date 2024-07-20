30199c056d0860f2248dd78634251c6d8b2b9ea7
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-21 05:25:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", ()=>{
+const button=document.getElementById("btn");
+const output= document.getElementById("output");
+
+button.addEventListener("click",()=>{
+	const text = document.getElementById("text").value;
+	const delay= document.getElementById("delay").value;
+	
+	
+       
+	
+		let display=setInterval(function(){
+		output.innerText=text;
+		 clearInterval(displayl);
+		}, parseInt(delay))
+	
+	
+})
+	
+})
